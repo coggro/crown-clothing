f68bb845233832a90e4f564df5f601023e7b3a37
Corey Gross <chg005@jefferson.edu>
2019-11-26 11:22:09 -0500

    Add Header component, THRUV71

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { Switch, Route } from "react-router-dom";
 
 import "./App.css";
 
+import Header from "./Components/Header/Header.component";
 import Homepage from "./Pages/Homepage/Homepage.component";
 import ShopPage from "./Pages/ShopPage/ShopPage.component";
 
 function App() {
 	return (
 		<div>
+			<Header />
 			<Switch>
 				<Route exact path="/" component={Homepage} />
 				<Route path="/shop" component={ShopPage} />

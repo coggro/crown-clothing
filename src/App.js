073d5d24b5c7dc192465877ed0f1356eeab39d62
CG <8009705+coggro@users.noreply.github.com>
2020-01-18 15:19:36 -0500

    Up to end of video 85

--- src/App.js
+++ src/App.js
@@ -7,18 +7,43 @@ import Header from "./Components/Header/Header.component";
 import Homepage from "./Pages/Homepage/Homepage.component";
 import ShopPage from "./Pages/ShopPage/ShopPage.component";
 import SignInAndSignUpPage from "./Pages/SignInAndSignUpPage/SignInAndSignUpPage.component";
+import { auth } from "./firebase/firebase.utils";
 
-function App() {
-	return (
-		<div>
-			<Header />
-			<Switch>
-				<Route exact path="/" component={Homepage} />
-				<Route path="/shop" component={ShopPage} />
-				<Route path="/signin" component={SignInAndSignUpPage} />
-			</Switch>
-		</div>
-	);
+class App extends React.Component {
+	constructor() {
+		super();
+
+		this.state ={
+			currentUser: null
+		}
+	}
+
+	unsubscribeFromAuth = null;
+
+	componentDidMount() {
+		this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
+			this.setState({currentUser: user});
+
+			console.log(user);
+		})
+	}
+
+	componentWillUnmount() {
+		this.unsubscribeFromAuth();
+	}
+	
+	render() {
+		return (
+			<div>
+				<Header currentUser={this.state.currentUser} />
+				<Switch>
+					<Route exact path="/" component={Homepage} />
+					<Route path="/shop" component={ShopPage} />
+					<Route path="/signin" component={SignInAndSignUpPage} />
+				</Switch>
+			</div>
+		);
+	}
 }
 
 export default App;

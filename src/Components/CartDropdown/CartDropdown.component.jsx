c7ac23e0109d44700056a1e12148b307c14ec5df
Corey Gross <chg005@jefferson.edu>
2020-05-13 15:53:20 -0400

    Working through 120

--- src/Components/CartDropdown/CartDropdown.component.jsx
+++ src/Components/CartDropdown/CartDropdown.component.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import { createStructuredSelector } from 'reselect'
 
 import CustomButton from '../CustomButton/CustomButton.component'
 import CartItem from '../CartItem/CartItem.component'
@@ -20,6 +21,6 @@ const CartDropdown = ({ cartItems }) => {
 	)
 }
 
-const mapStateToProps = state => selectCartItems(state)
+const mapStateToProps = createStructuredSelector({ cartItems: selectCartItems })
 
 export default connect(mapStateToProps)(CartDropdown)

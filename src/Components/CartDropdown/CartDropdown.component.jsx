4603a93d3b48754751434f5133064581a2b92144
Corey Gross <chg005@jefferson.edu>
2020-05-14 15:42:12 -0400

    In progress

--- src/Components/CartDropdown/CartDropdown.component.jsx
+++ src/Components/CartDropdown/CartDropdown.component.jsx
@@ -1,31 +1,39 @@
 import React from 'react'
-import {withRouter} from 'react-router-dom'
+import { withRouter } from 'react-router-dom'
 import { connect } from 'react-redux'
 import { createStructuredSelector } from 'reselect'
 
 import CustomButton from '../CustomButton/CustomButton.component'
 import CartItem from '../CartItem/CartItem.component'
 import { selectCartItems } from '../../redux/cart/cart.selectors'
+import { toggleCartHidden } from '../../redux/cart/cart.actions'
 
 import './CartDropdown.styles.scss'
 
-const CartDropdown = ({ cartItems }) => {
-  return (
-    <div className="cart-dropdown">
-      <div className="cart-items">
-        {cartItems.length ? (
-          cartItems.map(cartItem => (
-            <CartItem key={cartItem.id} item={cartItem} />
-          ))
-        ) : (
-          <span className="empty-message">Your cart is empty</span>
-        )}
-      </div>
-      <CustomButton>GO TO CHECKOUT</CustomButton>
-    </div>
-  )
+const CartDropdown = ({ cartItems, history, dispatch }) => {
+	return (
+		<div className='cart-dropdown'>
+			<div className='cart-items'>
+				{cartItems.length ? (
+					cartItems.map(cartItem => (
+						<CartItem key={cartItem.id} item={cartItem} />
+					))
+				) : (
+					<span className='empty-message'>Your cart is empty</span>
+				)}
+			</div>
+			<CustomButton
+				onClick={() => {
+					history.push('/checkout')
+					dispatch(toggleCartHidden())
+				}}
+			>
+				GO TO CHECKOUT
+			</CustomButton>
+		</div>
+	)
 }
 
 const mapStateToProps = createStructuredSelector({ cartItems: selectCartItems })
 
-export default withRouter(connect(mapStateToProps)(CartDropdown)
+export default withRouter(connect(mapStateToProps)(CartDropdown))
---
+++ src/Components/CheckoutItem/CheckoutItem.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import './CheckoutItem.styles.scss'
+
+function CheckoutItem() {
+	return (
+		<div className='checkout-item'>
+			<div className='image-container'></div>
+		</div>
+	)
+}
+
+export default CheckoutItem

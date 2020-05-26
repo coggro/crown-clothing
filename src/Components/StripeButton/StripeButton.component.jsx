1175ff20a49aba5bcba3387c2c2b4719e3ecbb71
CG <8009705+coggro@users.noreply.github.com>
2020-05-26 09:41:29 -0400

    In progress

--- src/Components/StripeButton/StripeButton.component.jsx
+++ src/Components/StripeButton/StripeButton.component.jsx
@@ -3,7 +3,7 @@ import StripeCheckout from 'react-stripe-checkout'
 
 const StripeButton = ({ price }) => {
   const priceForStripe = price * 100
-  const publishableKey = `pk_test_nonsense`
+  const publishableKey = `pk_test_8PPFhprvvlRIe7FCfnIH8gHs00t2TMvXXj`
 
   const onToken = token => {
     console.log(token)
